f3de96bbb0c5261f326fbbc73364d1575e8a6f71
ExceedPrince <tamas.bodorkos@hotmail.hu>
2021-02-26 15:58:07 +0100

    -

--- frontend/public/script.js
+++ frontend/public/script.js
@@ -2,9 +2,31 @@ const pageLoad = () => {
 	console.log("client is running");
 	let form = document.getElementById("uploadForm");
 
+	let formData = new FormData(form);
+
+
+
 	function notLoad (event) {
-		event.preventDefault();
+		event.preventDefault()
+		
 		console.log("We blocked the file upload process");
+		
+		Array(2).fill(1).map((item,index)=>{
+			return formData.append(`${document.querySelector('input').type}`, event.target[index].value);
+		})
+		
+		let allData = []
+		for (var value of formData.values()) {
+      allData.push(value);
+   }
+
+		console.log(allData)
+
+		fetch('https://localhost:8000/upload', {
+  		method: 'POST',
+  		body: allData
+		})
+
 	}
 
 	form.addEventListener("submit", notLoad);
